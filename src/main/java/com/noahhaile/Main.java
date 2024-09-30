2ba66f3a376d34968803d5d30f72b535e86dcc2d
NoahHaile <noahhaile456@gmail.com>
2024-09-30 19:40:23 +0300

    sample driver updated with comments

--- src/main/java/com/noahhaile/Main.java
+++ src/main/java/com/noahhaile/Main.java
@@ -3,13 +3,20 @@ package com.noahhaile;
 public class Main {
     public static void main(String[] args) {
 
+        // Basic Setup
         ShellCommandWrapper shellWrapper = new ShellCommandWrapperImpl();
         shellWrapper.setWorkingDirectory("/");
         shellWrapper.setEnvironmentVariable("KEY", "VALUE");
 
-        shellWrapper.readExecutionScript("demo.bat", TargetPlatform.ALL);
-        int exitCode = shellWrapper.execute();
+        // You can run scripts without a file but separate with newlines.
+//        shellWrapper.setExecutionScript(
+//                "echo hello\n" + "echo world"
+//                , TargetPlatform.ALL);
 
+        // If working inside an IDE, store file in the root of the project
+        shellWrapper.readExecutionScript("demo.bat", TargetPlatform.ALL);
+
+        int exitCode = shellWrapper.execute();
         String output = shellWrapper.getOutput();
         System.out.println("Exit Code: " + exitCode);
         System.out.println("Output: " + output);
